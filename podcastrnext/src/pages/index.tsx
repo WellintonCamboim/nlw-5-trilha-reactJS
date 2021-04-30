88263d97222867bdead1be5575f8d5425d6047ea
wellinton <wellinton.camboim@gmail.com>
2021-04-29 21:13:30 -0300

    [podcastr-012]-feature: Desenvolvendo a funçao convertDurationToTimeString

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -2,7 +2,8 @@ import { GetStaticProps } from 'next';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../server/api';
-
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+ 
 type Episode = {
   id: string;
   title: string;
@@ -28,29 +29,30 @@ export default function Home(props: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
     params: {
-    _limit: 12,
-    _sort: 'published_at',
-    _order: 'desc'
-  }
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
   })
-  
-const episodes = data.map(episode => {
-  return {
-    id: episode.id,
-    title: episode.title,
-    thumbnail: episode.thumbnail,
-    members: episode.members,
-    publishedAt: format(parseISO(episode.published_at), 'd MM yy', {locale: ptBR}),
-    duration: Number(episode.file.duration),
-    description: episode.description,
-    url: episode.file.url,
-  };
-})
 
-return {
-  props: {
-    episodes: data,
-  },
-  revalidate: 60 * 60 * 8,
-}
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
+
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
